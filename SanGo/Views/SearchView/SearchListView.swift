487d1ac62b4bd44406ce77cca7674f73efbb562b
HuynhLQ <huynhlq.dev@gmail.com>
2025-07-08 00:40:50 +0700

    SearchBarControl handler

--- SanGo/Views/SearchView/SearchListView.swift
+++ SanGo/Views/SearchView/SearchListView.swift
@@ -13,6 +13,12 @@ struct SearchListView: View {
     /// Data
     @ObservedObject var viewModel: SearchViewModel
 
+    /// State for layout view
+    @GestureState private var dragOffset = CGSize.zero
+    @State private var searchBarControlHidden: Bool = false
+    private let triggerDistance: CGFloat = 100
+
+
     /// State management values
     @State private var isShowSearchDetail: Bool = false
     @State private var isShowDateFilter: Bool = false
@@ -38,132 +44,167 @@ struct SearchListView: View {
         "Thêm phường,xã"
     ]
 
+    private var searchControl: some View {
+        VStack(spacing: 0) {
+            /// Welcome - notification - avatar(optional)
+            HStack {
+                /// Welcome
+                Text("Welcome")
+                    .font(.title).bold()
+                    .foregroundStyle(.white)
 
-    // MARK: BODY
-    var body: some View {
-        GeometryReader { gr in
-            VStack {
-                // HEADER
-                VStack(spacing: 0) {
-                    /// Welcome - notification - avatar(optional)
+                Spacer()
+
+                /// Notification button
+                Button(action: {
+                    print("Notification tapped")
+                }, label: {
+                    Image(systemName: "bell")
+                        .resizable()
+                        .foregroundStyle(.white)
+                        .frame(width: 22, height: 22)
+                        .darkShadow()
+                        .padding(.horizontal, 12)
+                })
+
+                /// Avatar button
+                Button(action: {
+                    print("Avatar tapped")
+                }, label: {
+                    UserImageView(imageURL: URL(string: "https://i.pravatar.cc/100"),
+                                  size: 32)
+                })
+            }.offset(x: 0, y: 8)
+
+            /// Search field
+            ZStack {
+                Button(action: {
+                    //TODO: create a action to show search view
+                    print("Search field tapped")
+                }, label: {
                     HStack {
-                        /// Welcome
-                        Text("Welcome")
-                            .font(.title).bold()
-                            .foregroundStyle(.white)
-
+                        Image(systemName: "magnifyingglass")
+                            .bold()
+                        Text(pannerText)
+                            .offset(CGSize(width: 0, height: textOffset))
                         Spacer()
-
-                        /// Notification button
-                        Button(action: {
-                            print("Notification tapped")
-                        }, label: {
-                            Image(systemName: "bell")
-                                .resizable()
-                                .foregroundStyle(.white)
-                                .frame(width: 22, height: 22)
-                                .darkShadow()
-                                .padding(.horizontal, 12)
-                        })
-
-                        /// Avatar button
-                        Button(action: {
-                            print("Avatar tapped")
-                        }, label: {
-                            UserImageView(imageURL: URL(string: "https://i.pravatar.cc/100"),
-                                          size: 32)
-                        })
-                    }.offset(x: 0, y: 8)
-
-                    /// Search field
-                    ZStack {
-                        Button(action: {
-                            //TODO: create a action to show search view
-                            print("Search field tapped")
-                        }, label: {
-                            HStack {
-                                Image(systemName: "magnifyingglass")
-                                    .bold()
-                                Text(pannerText)
-                                    .offset(CGSize(width: 0, height: textOffset))
-                                Spacer()
-                            }
-                            .font(.body)
-                            .foregroundStyle(.white)
-                            .padding(.horizontal)
-                        })
                     }
-                    .frame(height: searchButtonHeight)
-                    .background(Color(hex: "#1F7F7C"))
-                    .clipShape(.buttonBorder)
-                    .shadow(color: .black.opacity(0.3), radius: 6, x: 0, y: 4)
-                    .offset(x: 0, y: searchButtonHeight/2)
-                }
+                    .font(.body)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal)
+                })
+            }
+            .frame(height: searchButtonHeight)
+            .background(Color(hex: "#1F7F7C"))
+            .clipShape(.buttonBorder)
+            .shadow(color: .black.opacity(0.3), radius: 6, x: 0, y: 4)
+            .offset(x: 0, y: searchButtonHeight/2)
+        }
+    }
+
+    private var horizontalFilter: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                FilterButton(title: "Ngày", action: {
+                    withAnimation {isShowDateFilter = true}
+                })
+                FilterButton(title: "Khung giờ", action: {
+                    withAnimation {isShowTimeFilter = true}
+                })
+                FilterButton(title: "Loại sân", action: {
+                    withAnimation {isShowFieldTypeFilter = true}
+                })
+                FilterButton(title: "Giá tiền", action: {
+                    withAnimation {isShowPriceFilter = true}
+                })
+            }
+            .padding(.horizontal).padding(.vertical, 4)
+        }
+    }
+
+    // MARK: HEADER VIEW
+    private var searchBarControl: some View {
+        Group {
+            // SEARCHBAR
+            searchControl
                 .padding(.horizontal)
                 .background(Color(hex: "#67CAAF"))
                 .padding(.bottom, searchButtonHeight/2) // Cover search field - offset [y: 26]
 
-                // HORIZONTAL FILTER
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        FilterButton(title: "Ngày", action: {
-                            withAnimation {isShowDateFilter = true}
-                        })
-                        FilterButton(title: "Khung giờ", action: {
-                            withAnimation {isShowTimeFilter = true}
-                        })
-                        FilterButton(title: "Loại sân", action: {
-                            withAnimation {isShowFieldTypeFilter = true}
-                        })
-                        FilterButton(title: "Giá tiền", action: {
-                            withAnimation {isShowPriceFilter = true}
-                        })
+            // HORIZONTAL FILTER
+            horizontalFilter.scrollClipDisabled()
+        }
+
+        // DYNAMIC SEARCH BAR HANDLER
+        .onReceive(timer) { _ in
+            pannerIndex = (pannerIndex + 1) % pannerTextList.count
+            pannerText = pannerTextList[pannerIndex]
+        }
+        .onChange(of: pannerText) { _,_ in
+            withAnimation(.easeOut) {
+                textOffset = 10
+                asyncAfter(0.15, execute: {textOffset = 0})
+            }
+        }
+    }
+
+    // MARK: LIST FIELD
+    private var listField: some View {
+        Group {
+            /// Fields near you
+            HStack(spacing: 0) {
+                Text("Có ")
+                Text("\(viewModel.fields.count)").bold()
+                Text(" sân gần bạn")
+                Spacer()
+            }
+            .font(.subheadline)
+            .padding(.horizontal).padding(.vertical, 8)
+
+            /// List  field
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.fields, id: \.id) { field in
+                        NavigationLink(destination: FieldDetailView(field: field)) {
+                            FieldCard(field: field)
+                                .padding(.horizontal, 8)
+                                .padding(.bottom, 8)
+                                .listRowSeparator(.hidden)
+                                .listRowInsets(EdgeInsets())
+                        }
                     }
-                    .padding(.horizontal).padding(.vertical, 4)
-                }.scrollClipDisabled()
-
-                // LIST
-                /// Fields near you
-                HStack(spacing: 0) {
-                    Text("Có ")
-                    Text("\(viewModel.fields.count)").bold()
-                    Text(" sân gần bạn")
-                    Spacer()
-                }
-                .font(.subheadline)
-                .padding(.horizontal).padding(.vertical, 8)
-
-                /// List  field
-                List(viewModel.fields) { field in
-                    FieldCard(field: field)
-                        .background(
-                            NavigationLink("", destination: FieldDetailView(field: field))
-                        )
-                        .padding(.horizontal, 8)
-                        .padding(.bottom, 8)
-                        .listRowSeparator(.hidden)
-                        .listRowInsets(EdgeInsets())
-                }
-                .listStyle(.plain)
-            }
-            .background(Color(hex: "#F6F6F6"))
-
-            // FETCH DATA
-            .task {
-                viewModel.fetchFields()
-            }
-
-            // DYNAMIC SEARCH BAR HANDLER
-            .onReceive(timer) { _ in
-                pannerIndex = (pannerIndex + 1) % pannerTextList.count
-                pannerText = pannerTextList[pannerIndex]
-            }
-            .onChange(of: pannerText) { _,_ in
-                withAnimation(.easeOut) {
-                    textOffset = 10
-                    asyncAfter(0.15, execute: {textOffset = 0})
                 }
             }
+            .simultaneousGesture(
+                DragGesture().updating($dragOffset) { value, _, _ in
+                    if value.translation.height < (0 - triggerDistance) {
+                        withAnimation {searchBarControlHidden = true}
+                    }
+                    if value.translation.height > triggerDistance {
+                        withAnimation {searchBarControlHidden = false}
+                    }
+                }
+            )
+        }
+    }
+
+    // MARK: BODY VIEW
+    var body: some View {
+        VStack {
+            // HEADER
+            if !searchBarControlHidden {
+                searchBarControl
+                    .transition(.move(edge: .top).combined(with: .opacity))
+            }
+            // LIST
+            listField
+        }
+        .background(Color(hex: "#F6F6F6"))
+        .animation(.easeInOut(duration: 0.25), value: searchBarControlHidden)
+
+        // FETCH DATA
+        .task {
+            viewModel.fetchFields()
         }
     }
 }
